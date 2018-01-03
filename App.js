8af5861e4ae52e50a3f4df7be3059b48d9003eff
julien da silva <julien.da.silva@hotmail.com>
2018-01-03 17:08:50 +0100

    checkpoint

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './src/components/Header';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Header />
+        <View style={styles.header}>
+          <Icon name='ios-sunny' size={70} color='white'/>
+          <Text style={styles.temp}>24°</Text>
+        </View>
+        <View style={styles.body}>
+          <Text style={styles.title}>Build a fucking Weather App</Text>
+          <Text style={styles.subtitle}>Let's make it rain!</Text>
+        </View>
       </View>
     );
   }
@@ -15,8 +23,32 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFD017'
   },
+  header: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  temp: {
+    fontFamily: 'HelveticaNeue-Bold',
+    fontSize: 28,
+    color: 'white'
+  },
+  body: {
+    flex: 6,
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  title: {
+    fontFamily: 'HelveticaNeue-Bold',
+    fontSize: 55,
+    color: 'white'
+  },
+  subtitle: {
+    fontFamily: 'HelveticaNeue-Bold',
+    fontSize: 28,
+    color: 'white'
+  }
 });
