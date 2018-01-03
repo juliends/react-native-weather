f3e99f833c16e63dab613fae9fa2a425a28d2a9a
julien da silva <julien.da.silva@hotmail.com>
2018-01-03 17:53:17 +0100

    promise

--- App.js
+++ App.js
@@ -4,6 +4,26 @@ import Header from './src/components/Header';
 import Icon from 'react-native-vector-icons/Ionicons';
 
 export default class App extends Component {
+
+  componentDidMount(){
+    this.getLocation();
+    this.getWeather(21, 29);
+  }
+
+  getLocation() {
+    navigator.geolocation.getCurrentPosition(
+      (data) => console.log(data),
+      (error) => alert(error),
+      {timeout: 10000}
+    )
+  }
+
+  getWeather(lat, lng) {
+    const url = `api.openweathermap.org/data/2.5/weather?appid=fb618e8c4e64283120228836fd18bf36&lat=${lat}&${lng}=139`
+    fetch(url)
+      .then(data => console.log(data))  
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -12,7 +32,7 @@ export default class App extends Component {
           <Text style={styles.temp}>24°</Text>
         </View>
         <View style={styles.body}>
-          <Text style={styles.title}>Build a fucking Weather App</Text>
+          <Text style={styles.title}>Build a <Text style={{color: 'red'}}>fucking</Text> Weather App</Text>
           <Text style={styles.subtitle}>Let's make it rain!</Text>
         </View>
       </View>
@@ -23,13 +43,14 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFD017'
+    backgroundColor: '#FFD017',
   },
   header: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginTop: 30
   },
   temp: {
     fontFamily: 'HelveticaNeue-Bold',
@@ -39,16 +60,18 @@ const styles = StyleSheet.create({
   body: {
     flex: 6,
     justifyContent: 'flex-end',
-    alignItems: 'flex-start'
+    alignItems: 'flex-start',
+    margin: 10
   },
   title: {
     fontFamily: 'HelveticaNeue-Bold',
     fontSize: 55,
-    color: 'white'
+    color: 'white',
+    marginBottom: 30
   },
   subtitle: {
     fontFamily: 'HelveticaNeue-Bold',
-    fontSize: 28,
+    fontSize: 20,
     color: 'white'
   }
 });
